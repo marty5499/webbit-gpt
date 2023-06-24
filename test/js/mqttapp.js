e1e36b1bee5b555b4425de94710a52ffe02a343a
marty <marty.shu@gmail.com>
2023-06-24 13:46:47 +0800

    set topic

--- test/js/mqttapp.js
+++ test/js/mqttapp.js
@@ -12,7 +12,7 @@ class MQTTApp {
         };
         this.onConnectPromise = null;
         this.subscriptions = {}; // 存儲訂閱關係的對象
-        var topic = "kn@chat";
+        var topic = "kn@chat-staging";
         this.pubTopic = topic + '_prompt/' + userId;
         this.respTopic_cb = topic + "_completion/" + this.userId;
         this.respTopic_end = topic + "_completion_end/" + this.userId;
